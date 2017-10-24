c07cec9b69992fd479321e116c2b7731c20be067
jialin <jialin@ifastfinancial.com.cn>
2017-10-24 14:20:00 +0800

    update

--- examples/simple.js
+++ examples/simple.js
@@ -1,44 +1,53 @@
 // use jsx to render html, do not modify simple.html
 
-import 'rmc-pull-to-refresh/assets/index.less';
-import PullToRefresh from 'rmc-pull-to-refresh';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Icon from 'antd/lib/icon';
-import 'antd/lib/icon/style/css';
+import "rmc-pull-to-refresh/assets/index.less";
+import PullToRefresh from "rmc-pull-to-refresh";
+import React from "react";
+import ReactDOM from "react-dom";
+import Icon from "antd/lib/icon";
+import "antd/lib/icon/style/css";
 
 let count = 1;
 const App = React.createClass({
   getInitialState() {
     return {
       items: [
-        <div key={`item-${count}`} style={{ height: 50 }}>Item {count++}</div>,
-      ],
+        <div key={`item-${count}`} style={{ height: 50 }}>
+          Item {count++}
+        </div>
+      ]
     };
   },
   loadingFunction() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (this.addItem()) {
-          resolve();
-        } else {
-          reject();
-        }
-      }, 500);
-    });
+    return [
+      new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (this.addItem()) {
+            resolve();
+          } else {
+            reject();
+          }
+        }, 500);
+      })
+    ];
   },
   addItem() {
-    this.state.items.push(<div key={`item-${count}`} style={{ height: 100 }}>Item {count++}</div>);
+    this.state.items.push(
+      <div key={`item-${count}`} style={{ height: 100 }}>
+        Item {count++}
+      </div>
+    );
     this.setState({
-      items: this.state.items,
+      items: this.state.items
     });
     return true;
   },
   render() {
     return (
       <div>
-        <style dangerouslySetInnerHTML={{
-          __html: `
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
             .rmc-pull-to-refresh .pull {
                 display: block;
              }
@@ -51,30 +60,34 @@ const App = React.createClass({
             .rmc-pull-to-refresh.rmc-pull-to-refresh-active .release {
               display: block;
             }
-          `,
-        }}
+          `
+          }}
         />
         <PullToRefresh
           loadingFunction={this.loadingFunction}
           icon={
             <div style={{ marginTop: 15 }}>
               <div className="pull">
-                <Icon type="down"/> pull down to refresh
+                <Icon type="down" /> pull down to refresh
               </div>
               <div className="release">
-                <Icon type="up"/> release to refresh
+                <Icon type="up" /> release to refresh
               </div>
             </div>
           }
-          loading={<div style={{ marginTop: 15 }}><Icon type="loading"/> Loading</div>}
+          loading={
+            <div style={{ marginTop: 15 }}>
+              <Icon type="loading" /> Loading
+            </div>
+          }
           className="forTest"
           contentClassName="for-test"
           style={{
             height: 300,
-            textAlign: 'center',
-            width: '90%',
-            margin: '0 auto',
-            border: '1px solid red',
+            textAlign: "center",
+            width: "90%",
+            margin: "0 auto",
+            border: "1px solid red"
           }}
         >
           <h3>Pull down to refresh</h3>
@@ -82,7 +95,7 @@ const App = React.createClass({
         </PullToRefresh>
       </div>
     );
-  },
+  }
 });
 
-ReactDOM.render(<App />, document.getElementById('__react-content'));
+ReactDOM.render(<App />, document.getElementById("__react-content"));
--- src/PullToRefresh.jsx
+++ src/PullToRefresh.jsx
@@ -29,16 +29,15 @@ class PullToRefresh extends React.Component {
   };
 
   componentDidMount() {
-    const { props, refs } = this;
-    const { prefixCls } = props;
-    const containerClassList = refs.container.classList;
-    this.domScroller = new DOMScroller(refs.content, {
+    const { prefixCls } = this.props;
+    const containerClassList = this.container.classList;
+    this.domScroller = new DOMScroller(this.content, {
       ...defaultScrollerOptions,
-      ...props.scrollerOptions
+      ...this.props.scrollerOptions
     });
     const scroller = this.domScroller.scroller;
     scroller.activatePullToRefresh(
-      props.distanceToRefresh,
+      this.props.distanceToRefresh,
       () => {
         containerClassList.add(`${prefixCls}-active`);
       },
@@ -48,7 +47,7 @@ class PullToRefresh extends React.Component {
       },
       () => {
         containerClassList.add(`${prefixCls}-loading`);
-        Promise.all([props.loadingFunction(), fake()]).then(
+        Promise.all([this.props.loadingFunction(), fake()]).then(
           this.finishPullToRefresh,
           this.finishPullToRefresh
         );
@@ -83,14 +82,24 @@ class PullToRefresh extends React.Component {
       <div
         className={`${className} ${prefixCls}`}
         style={style}
-        ref="container"
+        ref={container => {
+          this.container = container;
+        }}
       >
         <div
-          ref="content"
+          ref={content => {
+            this.content = content;
+          }}
           className={`${prefixCls}-content ${contentClassName}`}
           style={contentStyle}
         >
-          <div key="ptr" ref="ptr" className={`${prefixCls}-ptr`}>
+          <div
+            key="ptr"
+            ref={ptr => {
+              this.ptr = ptr;
+            }}
+            className={`${prefixCls}-ptr`}
+          >
             <div className={`${prefixCls}-ptr-icon`}>{icon}</div>
             <div className={`${prefixCls}-ptr-loading`}>{loading}</div>
           </div>
